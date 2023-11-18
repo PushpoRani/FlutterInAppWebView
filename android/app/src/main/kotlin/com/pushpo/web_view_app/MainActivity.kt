e746f43675ff9c5ecbd4dc26c9bbb097fdd91f2b
PushpoRani <pushporani7@gmail.com>
2023-11-19 01:29:20 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/pushpo/web_view_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pushpo.web_view_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _WebViewState();
+}
+
+class _WebViewState extends State<MyApp> {
+  double _progress = 0;
+  late InAppWebViewController  inAppWebViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: ()async{
+        var isLastPage = await inAppWebViewController.canGoBack();
+        if(isLastPage){
+          inAppWebViewController.goBack();
+          return false;
+        }
+        return true;
+      },
+      child: SafeArea(
+        child: Scaffold(
+          body: Stack(
+            children: [
+              InAppWebView(
+                initialUrlRequest: URLRequest(
+                    url: Uri.parse("https://protocoderspoint.com/")
+                ),
+                onWebViewCreated: (InAppWebViewController controller){
+                  inAppWebViewController = controller;
+                },
+                onProgressChanged: (InAppWebViewController controller , int progress){
+                  setState(() {
+                    _progress = progress / 100;
+                  });
+                },
+              ),
+              _progress < 1 ? Container(
+                child: LinearProgressIndicator(
+                  value: _progress,
+                ),
+              ):SizedBox()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
