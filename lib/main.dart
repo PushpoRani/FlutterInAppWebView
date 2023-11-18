f5de46a0999f61cf2b1e6d53c7daf1e4bf4908ff
PushpoRani <pushporani7@gmail.com>
2023-11-19 02:16:40 +0600

    modified url, fix issue with material widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,45 +15,52 @@ class MyApp extends StatefulWidget {
 
 class _WebViewState extends State<MyApp> {
   double _progress = 0;
-  late InAppWebViewController  inAppWebViewController;
+  late InAppWebViewController inAppWebViewController;
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: ()async{
-        var isLastPage = await inAppWebViewController.canGoBack();
-        if(isLastPage){
-          inAppWebViewController.goBack();
-          return false;
-        }
-        return true;
-      },
-      child: SafeArea(
-        child: Scaffold(
-          body: Stack(
-            children: [
-              InAppWebView(
-                initialUrlRequest: URLRequest(
-                    url: Uri.parse("https://protocoderspoint.com/")
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Builder(builder: (BuildContext context)
+    {
+      return WillPopScope(
+        onWillPop: () async {
+          var isLastPage = await inAppWebViewController.canGoBack();
+          if (isLastPage) {
+            inAppWebViewController.goBack();
+            return false;
+          }
+          return true;
+        },
+        child: SafeArea(
+          child: Scaffold(
+            body: Stack(
+              children: [
+                InAppWebView(
+                  initialUrlRequest: URLRequest(
+                      url: Uri.parse("https://barikoi.com/")
+                  ),
+                  onWebViewCreated: (InAppWebViewController controller) {
+                    inAppWebViewController = controller;
+                  },
+                  onProgressChanged: (InAppWebViewController controller,
+                      int progress) {
+                    setState(() {
+                      _progress = progress / 100;
+                    });
+                  },
                 ),
-                onWebViewCreated: (InAppWebViewController controller){
-                  inAppWebViewController = controller;
-                },
-                onProgressChanged: (InAppWebViewController controller , int progress){
-                  setState(() {
-                    _progress = progress / 100;
-                  });
-                },
-              ),
-              _progress < 1 ? Container(
-                child: LinearProgressIndicator(
-                  value: _progress,
-                ),
-              ):SizedBox()
-            ],
+                _progress < 1 ? Container(
+                  child: LinearProgressIndicator(
+                    value: _progress,
+                  ),
+                ) : SizedBox()
+              ],
+            ),
           ),
         ),
-      ),
+      );
+    })
     );
   }
 }
